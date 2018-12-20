fe15dc01719b79f5634b8677b2154aa90858c75e
Hugo Fernandez <hugo.fernandez@telecomnancy.net>
2018-12-20 18:08:04 -0500

    added view active battle component

--- src/app/component/view-active-battles/view-active-battles.component.ts
+++ src/app/component/view-active-battles/view-active-battles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Battle } from 'src/app/types/battle';
 import { BattleService } from 'src/app/services/battle.service';
+import { CharacterMarvelServiceService } from 'src/app/services/character-marvel-service.service';
 
 @Component({
   selector: 'app-view-active-battles',
@@ -9,15 +10,45 @@ import { BattleService } from 'src/app/services/battle.service';
 })
 export class ViewActiveBattlesComponent implements OnInit {
 
-  constructor(private battleService: BattleService) { }
+  constructor(private battleService: BattleService, private marvelService: CharacterMarvelServiceService) { }
   battles: Battle[];
+
 
   ngOnInit() {
     this.getActiveBattles();
   }
 
   getActiveBattles() {
-    this.battleService.getAllActiveBattles().subscribe(data => this.battles = data);
+    this.battleService.getAllActiveBattles().subscribe(data => { this.battles = data; this.getCharacterInfoForBattles(); });
+  }
+
+  getCharacterInfoForBattles() {
+
+    // add the description, name, and thumbnail of each team1 character from the Marvel API
+    this.battles.forEach(battle => {
+      battle.team1.characters.forEach(char => {
+        this.marvelService.getCharacterById(char.characterId).subscribe(data => {
+          char.description = data.data.results[0].description;
+          char.name = data.data.results[0].name;
+          char.thumbnail = data.data.results[0].thumbnail;
+        });
+      });
+
+      // add the description, name, and thumbnail of each team2 character from the Marvel API
+      battle.team2.characters.forEach(char => {
+        this.marvelService.getCharacterById(char.characterId).subscribe(data => {
+          char.description = data.data.results[0].description;
+          char.name = data.data.results[0].name;
+          char.thumbnail = data.data.results[0].thumbnail;
+        });
+      });
+    });
+
+    console.log(this.battles);
+  }
+
+  battleClick() {
+
   }
 
 }
